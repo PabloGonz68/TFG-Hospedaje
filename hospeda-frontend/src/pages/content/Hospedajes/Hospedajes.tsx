2cc7b9bf9fd16e87e50c886cec972f16b989bfb9
PabloGonz <pablogonzalezsilva6@gmail.com>
2025-05-25 18:27:31 +0200

    Hospedajes mostrandose inicio

--- hospeda-frontend/src/pages/content/Hospedajes/Hospedajes.tsx
+++ hospeda-frontend/src/pages/content/Hospedajes/Hospedajes.tsx
@@ -1,11 +1,86 @@
+import { useEffect, useState } from "react";
+
+interface Hospedaje {
+    nombre: string;
+    direccion: string;
+    codigoPostal: string;
+    ciudad: string;
+    pais: string;
+    capacidad: number;
+    tipoZona: 'CIUDAD' | 'PUEBLO';
+    descripcion: string;
+    ubicacion: string;
+    visible: boolean;
+}
+
 const Hospedajes = () => {
+
+    const [hospedajes, setHospedajes] = useState<Hospedaje[]>([]);
+
+    useEffect(() => {
+        const fetchHospedajes = async () => {
+            const token = localStorage.getItem("token");
+
+            if (!token) return;
+            try {
+                const response = await fetch("http://localhost:8080/hospedaje/", {
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
+
+                if (!response.ok) throw new Error("Error al obtener los hospedajes");
+                const data = await response.json();
+                setHospedajes(data);
+            } catch (error) {
+                console.error("Error al obtener los hospedajes", error);
+            }
+        }
+        fetchHospedajes();
+    }, []);
+
+
 
 
 
     return (
-        <div>
-            <h1>hospedajes</h1>
+        <div className="container mt-30 mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-6 text-center">Hospedajes</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {
+                    hospedajes.length === 0 ? (
+                        <div className="h-screen w-full flex items-center justify-center">
+                            <h2 className="text-xl font-semibold">No hay hospedajes disponibles</h2>
+                        </div>
+
+                    ) :
+                        (
+                            hospedajes.map((h, index) => (
+                                <div
+                                    key={index}
+                                    className="bg-white shadow-md rounded-2xl p-5 hover:shadow-lg transition"
+                                >
+                                    <h2 className="text-xl font-semibold mb-2">{h.nombre}</h2>
+                                    <p className="text-gray-700"><span className="font-medium">Dirección:</span> {h.direccion}, {h.codigoPostal}</p>
+                                    <p className="text-gray-700"><span className="font-medium">Ciudad:</span> {h.ciudad}, {h.pais}</p>
+                                    <p className="text-gray-700"><span className="font-medium">Capacidad:</span> {h.capacidad} personas</p>
+                                    <p className="text-gray-700"><span className="font-medium">Zona:</span> {h.tipoZona}</p>
+                                    <p className="text-gray-600 mt-2">{h.descripcion}</p>
+                                    <a
+                                        href={h.ubicacion}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="inline-block mt-3 text-blue-600 hover:underline"
+                                    >
+                                        Ver en mapa
+                                    </a>
+                                </div>
+                            )
+
+                            ))}
+            </div>
         </div>
     )
 }
+
 export default Hospedajes;
--- hospeda-frontend/src/routes/AppRoutes.tsx
+++ hospeda-frontend/src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Register from "@/pages/auth/Register";
 import Login from "@/pages/auth/Login";
 import MainLayout from "@/layouts/MainLayout";
 import Perfil from "@/pages/content/Perfil";
+import Hospedajes from "@/pages/content/Hospedajes/Hospedajes";
 
 function AppRoutes() {
     return (
@@ -13,6 +14,9 @@ function AppRoutes() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/perfil" element={<MainLayout><Perfil /></MainLayout>} />
+                <Route path="/hospedajes" element={<MainLayout><Hospedajes /></MainLayout>}></Route>
+                <Route path="*" element={<h1>404</h1>} />
+
             </Routes>
         </BrowserRouter>
     );
